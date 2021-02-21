8137bac52e811aaab4bf4442aad4eb4e4d2e1773
Stuler <peter.jurek@gmail.com>
2021-02-21 07:31:58 +0100

    php started

---
+++ frontend/partials/footer.php
@@ -0,0 +1,19 @@
+<footer class="nav-bar footer-bar">
+        <div class="container footercontainer">
+            <h1 class="logo">
+                <a href="">MAKAY</a> 
+            </h1>
+            <article class="footer-text">
+                Copyright © 2021, Peter Jurek,  All rights reserved.
+            </article>
+            <nav class="navigation footnav group">
+                <ul class="menu footermenu">
+                    <li><a href=#terms_of_use>Terms of use</a></li>
+                    <li><a href=#privacy_policy>Privacy policy</a></li>
+                </ul>
+            </nav>
+        </div>
+    </footer>
+    <script src="js/app.js"></script>
+    </body>
+</html>
---
+++ frontend/partials/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>MAKAY</title>
+</head>
+<body>
+
+    <header class="nav-bar">
+        <div class="container">
+            <h1 class="logo">
+                <a href="">MAKAY</a> 
+            </h1>
+            <nav class="navigation group">
+                <ul class="menu  mainmenu">
+                    <li><a href=#feat>Features</a></li>
+                    <li><a href=#how>How it works</a></li>
+                    <li><a href=#author>About author</a></li>
+                    <li><a href=#blog>Blog</a></li>
+                </ul>
+                <ul class="menu subscribe">
+                    <li><a href=#rese>Log In</a></li>
+                    <li><a href=#desc">Subscribe</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
