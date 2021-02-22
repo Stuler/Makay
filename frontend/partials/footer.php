83cca98b34a0f9408f97501c6d086ba13a19d721
Stuler <peter.jurek@gmail.com>
2021-02-22 14:33:24 +0100

    css cleanup of footer and header sections

--- frontend/partials/footer.php
+++ frontend/partials/footer.php
@@ -1,12 +1,12 @@
-<footer class="nav-bar footer-bar">
-        <div class="container footercontainer">
+<footer class="nav-bar footer-bar footnav">
+        <div class="container ">
             <h1 class="logo">
                 <a href="">MAKAY</a> 
             </h1>
             <article class="footer-text">
                 Copyright © 2021, Peter Jurek,  All rights reserved.
             </article>
-            <nav class="navigation footnav group">
+            <nav class="navigation  group">
                 <ul class="menu footermenu subscribe">
                     <li><a href="terms_of_use.php">Terms of use</a></li>
                     <li><a href="privacy_policy.php">Privacy policy</a></li>
